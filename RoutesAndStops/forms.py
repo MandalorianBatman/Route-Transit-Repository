e9544dbcfa89f8030e86fffc3ef1ee55c0160061
zedkasat <sid.kasat@gmail.com>
2020-02-10 21:28:26 +0530

    Minor UI changes in the Route and Stops Form

--- RoutesAndStops/forms.py
+++ RoutesAndStops/forms.py
@@ -1,25 +1,54 @@
 from django import forms
 from RoutesAndStops.models import Route, Stop
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Row, Column, HTML, Submit
 
 
 class RouteForm(forms.ModelForm):
-    # stops = [(stop.name, stop.name) for stop in Stop.objects.all()]
     list_of_stops = forms.ModelMultipleChoiceField(queryset=Stop.objects.all(), required=False)
-    # list_of_stops = forms.MultipleChoiceField(choices=[(stop.name, stop.name) for stop in Stop.objects.all()], required=False)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Row(
+                Column('name', css_class='col-6 mr-3'),
+                Column('status', css_class='col-5 ml-3'),
+            ),
+            Row(
+                Column('direction', css_class='col-6 mr-3'),
+                Column('type', css_class='col-5 ml-3'),
+            ),
+            Row(
+                Column('list_of_stops', css_class='col-6 mr-4'),
+                Column(
+                    HTML("""<label for="sort_stops">Selected stops (drag to rearrange)</label>
+                        <div id="sort_stops"></div><input hidden name="stops_in_sequence" id="stops_in_sequence">""")
+                ),
+            ),
+            Submit('submit', 'Submit')
+        )
 
     class Meta:
         model = Route
         fields = ('name', 'direction', 'status', 'type')
 
-    # def __init__(self, *args, **kwargs):
-    #     super().__init__()
-    #     self.fields['name'].widget = forms.TextInput(attrs={
-    #         'id': 'route_name_id',
-    #         'name': 'route'
-    #     })
-
 
 class StopsForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Row(
+                Column('name', css_class='col-11 ml-4')
+            ),
+            Row(
+                Column('latitudes', css_class='col-5 mr-5 ml-4'),
+                Column('longitudes', css_class='col-5 ml-4')
+            ),
+            Submit('submit', 'Submit', css_class='ml-4')
+        )
+
     class Meta:
         model = Stop
         fields = '__all__'
